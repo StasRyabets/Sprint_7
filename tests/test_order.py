212146ba07bb2756b39c0c45a55cfe0384673566
Stas Ryabets <stas.ryabets@mail.ru>
2023-11-07 12:25:23 +0500

    delete cycle from test_get_order_with_filters

--- tests/test_order.py
+++ tests/test_order.py
@@ -15,7 +15,6 @@ class TestOrder:
     def test_create_order_with_specific_color(self, first_color, second_color):
         assert isinstance(create_order_with_specific_color(first_color, second_color), int) 
 
-    def test_get_order_with_filters(self):
-        orders = get_one_order_from_list_with_first_metro_station()
-        for order in orders:
-            assert isinstance(order['id'], int)
+    def test_get_order_with_filters(self): 
+        order = get_one_order_from_list_with_first_metro_station()[0] 
+        assert isinstance(order['id'], int)
--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
 reg_url = 'https://qa-scooter.praktikum-services.ru/api/v1/courier'
 login_url = 'https://qa-scooter.praktikum-services.ru/api/v1/courier/login'
 order_url = 'https://qa-scooter.praktikum-services.ru/api/v1/orders'
-order_with_filters_url= 'https://qa-scooter.praktikum-services.ru/api/v1/orders?limit=5&page=0&nearestStation=["1"]'
+order_with_filters_url= 'https://qa-scooter.praktikum-services.ru/api/v1/orders?limit=1&page=0&nearestStation=["1"]'
