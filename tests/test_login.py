2c96a997c0485a1a1cb4e7a62a6be5ddc6d21283
Stas Ryabets <stas.ryabets@mail.ru>
2023-11-07 12:01:43 +0500

    split test_login_with_typo into two tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -23,21 +23,14 @@ class TestLogin:
         assert login_user(
             login, password) == Login.response_400
 
-    @pytest.mark.parametrize(
-        'chars_remove_from_login, chars_remove_from_password',
-        [
-            [0, 1],
-            [1, 0],
-        ]
-    )
-    def test_login_with_typo(self, chars_remove_from_login, chars_remove_from_password):
-        login = generate_random_string(8)
-        password = generate_random_string(9)
+    def test_login_with_typo_in_login(self):
+        login = generate_random_string(10)
+        password = generate_random_string(11)
         register_new_courier_with_specific_data(login, password)
-        if chars_remove_from_login == 0:
-            login_with_typo = login_user(
-                login, password[:-chars_remove_from_password])
-        elif chars_remove_from_password == 0:
-            login_with_typo = login_user(
-                login[:-chars_remove_from_login], password)
-        assert login_with_typo == Login.response_404
+        assert login_user(login[:-1], password) == Login.response_404
+
+    def test_login_with_typo_in_password(self):
+        login = generate_random_string(10)
+        password = generate_random_string(11)
+        register_new_courier_with_specific_data(login, password)
+        assert login_user(login, password[:-1]) == Login.response_404
